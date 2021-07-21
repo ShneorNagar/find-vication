71c9657b4b15f09c728e30b941a4df96ed9070e7
ShneorNagar <33627109+ShneorNagar@users.noreply.github.com>
2021-07-22 00:02:16 +0300

    Update getVication.js

--- getVication.js
+++ getVication.js
@@ -1,10 +1,18 @@
 var resSel = '#nofeshApp > div.searchDealsPage.page > section.filterSearchSection > div > ul > section > h3'
+
 var resEl = document.querySelectorAll(resSel);
 var resVal = resEl[0].innerHTML;
 var searchBtn = document.getElementById('searchButton');
 
+searchBtn.click();
+
 var interval = setInterval(()=>{
-    searchBtn.click();
-resEl = document.querySelectorAll(resSel);
-if(resEl[0] == undefined) clearInterval(interval)
+    resEl = document.querySelectorAll(resSel);
+
+    if( resEl[0] == undefined ) {
+        clearInterval(interval)
+    }
+    else {
+        searchBtn.click();
+    }
 }, 1000)
